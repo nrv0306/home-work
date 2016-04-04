0c6a99e81df14dfa84872d7fdc20d6f6dd80d3f0
nrv0306 <nrv0306@gmail.com>
2016-04-04 10:42:07 -0400

    Added files via upload

---
+++ idk/js/scripts.js
@@ -0,0 +1,13 @@
+// Scripts for rows.html
+
+$('.animate-on-mouseenter').on('mouseenter', function() {
+	$(this).toggleClass("animated hinge");
+});
+
+$('.Animate-on-mouseenter').on('mouseenter', function() {
+	$(this).toggleClass("animated jello");
+});
+
+$('.ANimate-on-mouseenter').on('mouseenter', function() {
+	$(this).toggleClass("animated swing");
+})
